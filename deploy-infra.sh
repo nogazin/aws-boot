b434e842db923fc9140c08d0d190f739d3986ebb
Justin Allen <justinallen.ch@gmail.com>
2020-12-02 19:27:48 +0100

    Updates

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-STACK_NAME=awsbootstrap
+STACK_NAME=awsbootstrap2
 REGION=us-east-1
 CLI_PROFILE=awsbootstrap
 
@@ -18,23 +18,29 @@ GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
 # Deploy s3
 echo -e "\n\n========== Deploying setup.yml =========="
 aws cloudformation deploy \
---region $REGION \
---profile $CLI_PROFILE \
---stack-name $STACK_NAME \
---template-file setup.yml \
---no-fail-on-empty-changeset \
---capabilities CAPABILITY_NAMED_IAM \
---parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
+    --region $REGION \
+    --profile $CLI_PROFILE \
+    --stack-name $STACK_NAME \
+    --template-file setup.yml \
+    --no-fail-on-empty-changeset \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 echo -e "\n\n========== Deploying main.yml =========="
 aws cloudformation deploy \
---region $REGION \
---profile $CLI_PROFILE \
---stack-name $STACK_NAME \
---template-file main.yml \
---no-fail-on-empty-changeset \
---capabilities CAPABILITY_NAMED_IAM \
---parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+    --region $REGION \
+    --profile $CLI_PROFILE \
+    --stack-name $STACK_NAME \
+    --template-file main.yml \
+    --no-fail-on-empty-changeset \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    GitHubRepo=$GH_REPO \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 
 # If deploy succeeded, show the DNS name of the created instance
