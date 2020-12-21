967ed0c52afc2e6aba60ea76664b9838fc059173
Justin Allen <justinallen.ch@gmail.com>
2020-12-21 21:28:18 +0100

    Add HTTPS listener; Add cert yo launch script

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOMAIN=justinthecloud.co.uk
-STACK_NAME=awsbootstrap
+STACK_NAME=awsbootstrap123
 REGION=us-east-1
 CLI_PROFILE=awsbootstrap
 
@@ -11,6 +11,8 @@ CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 GH_BRANCH=master
 
 CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
+
+CERT=`aws acm list-certificates --region $REGION --profile $CLI_PROFILE --output text --query "CertificateSummaryList[?DomainName=='$DOMAIN'].CertificateArn | [0]"`
 
 # Generate a personal access token with repo and admin:repo_hook
 GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
@@ -57,6 +59,7 @@ aws cloudformation deploy \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
     Domain=$DOMAIN \
+    Certificate=$CERT \
     EC2InstanceType=$EC2_INSTANCE_TYPE \
     GitHubOwner=$GH_OWNER \
     GitHubBranch=$GH_BRANCH \
