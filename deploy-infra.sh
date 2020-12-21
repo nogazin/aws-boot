83bce0d33dfb647ac82c5a4ff19666068786e5e9
Justin Allen <justinallen.ch@gmail.com>
2020-12-21 12:36:46 +0100

    Nested Stack

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -9,6 +9,8 @@ AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Acco
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 GH_BRANCH=master
 
+CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
+
 # Generate a personal access token with repo and admin:repo_hook
 GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
 GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
@@ -20,18 +22,36 @@ echo -e "\n\n========== Deploying setup.yml =========="
 aws cloudformation deploy \
     --region $REGION \
     --profile $CLI_PROFILE \
-    --stack-name $STACK_NAME \
+    --stack-name $STACK_NAME-setup \
     --template-file setup.yml \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
-    --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
+    --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET \
+    CloudFormationBucket=$CFN_BUCKET
+
+echo -e "\n\n========== Packaging main.yml =========="
+mkdir -p ./cfn_output
+
+PACKAGE_ERR="$(aws cloudformation package \
+--region $REGION \
+--profile $CLI_PROFILE \
+--template main.yml \
+--s3-bucket $CFN_BUCKET \
+--output-template-file ./cfn_output/main.yml 2>&1)"
+
+if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
+echo "ERROR while running 'aws cloudformation package' command:"
+echo $PACKAGE_ERR
+exit 1
+fi
 
 echo -e "\n\n========== Deploying main.yml =========="
 aws cloudformation deploy \
     --region $REGION \
     --profile $CLI_PROFILE \
     --stack-name $STACK_NAME \
-    --template-file main.yml \
+    --template-file ./cfn_output/main.yml \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
